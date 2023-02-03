1740a7dd40858f3b9652254a1eafff14155885d1
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-02-03 13:49:11 -0300

    feat: create a song component

--- lyrical-project/client/components/SongList.js
+++ lyrical-project/client/components/SongList.js
@@ -5,14 +5,22 @@ import { graphql } from "react-apollo";
 class SongList extends Component {
   renderSongs() {
     return this.props.data.songs.map((song) => {
-      return <li>{song.title}</li>;
+      return (
+        <li key={song.id} className="collection-item">
+          {song.title}
+        </li>
+      );
     });
   }
   render() {
     if (this.props.data.loading) {
       return <div>Loading...</div>;
     }
-    return <div>{this.renderSongs()}</div>;
+    return (
+      <div>
+        <ul className="collection">{this.renderSongs()}</ul>
+      </div>
+    );
   }
 }
 
